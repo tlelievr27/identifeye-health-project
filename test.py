e389b125d92c3e8f32e208e8653bd5d360f35b48
tlelievr27 <tlelievr@purdue.edu>
2024-03-21 12:53:00 -0400

    Added testing file + function

---
+++ test.py
@@ -0,0 +1,24 @@
+from read_file import parse_file
+
+def run_tests(input_files, expected_outputs):
+
+    for i in range(0, len(input_files)):
+        return_str = parse_file(input_files[i])
+        with open(expected_outputs[i]) as output:
+            output = output.read()
+            print("Expected Output:\n" + output)
+            assert return_str == output, "Input does not match expected output"
+
+    print("All tests passed")
+
+if __name__ == '__main__':
+    #Responsible for running through the tests in the tests folder
+    #And asserting their output matches the intended format
+
+    #The test files I wrote look at edge cases for each individual command type
+    #plus any interactions between commands (ie deleting a patient then being able to add an exam number they used to have)
+
+    #Set up the parse_file function to return the string it prints to stdout to make testing easier
+    test_files = ['./tests/example.txt', './tests/add_exams.txt', './tests/del_exams.txt', './tests/del_patients.txt']
+    expected_files = ['./tests/example_expected.txt', './tests/add_exams_expected.txt', './tests/del_exams_expected.txt', './tests/del_patients_expected.txt']
+    run_tests(test_files, expected_files)
